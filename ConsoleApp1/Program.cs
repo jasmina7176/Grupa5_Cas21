2f163fe7bc8d48e33a22913a7fb76aa9a4f5cfe4
Jasmina Zivojinovic <jasmina.zivojinovic@outlook.com>
2019-11-18 12:36:17 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? num1 = null;
+            bool? isItTrue = true;
+
+            if (isItTrue != null)
+            {
+                if (isItTrue == true)
+                {
+                    Console.WriteLine("Value is TRUE.");
+                }
+                else 
+                {
+                    Console.WriteLine("Value is FALSE.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Value is not chosen.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
